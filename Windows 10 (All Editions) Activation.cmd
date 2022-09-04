f32dd818b07595f1ec35f7662cefbe6d45aa8346
MirzAbi <100478671+mirzakk@users.noreply.github.com>
2022-09-04 18:30:36 +0300

    Add files via upload

---
+++ Windows 10 (All Editions) Activation.cmd
@@ -0,0 +1,416 @@
+@echo off
+
+title Windows 10 (All Editions) Activation
+
+cls
+
+Echo.
+
+Echo.
+
+Echo                 LUTFEN YONETiCi OLARAK CALISTIRIN
+
+Echo.
+
+Echo.
+
+Echo 1. Home/Core
+
+Echo 2. Home/Core (Country Specific)
+
+Echo 3. Home/Core (Single Language)
+
+Echo 4. Home/Core N 
+
+Echo 5. Professional
+
+Echo 6. Professional N
+
+Echo 7. Enterprise
+
+Echo 8. Enterprise N
+
+Echo 9. Education
+
+Echo 10. Education N
+
+Echo 11. Enterprise 2015 LTSB 
+
+Echo 12. Enterprise 2015 LTSB N
+
+Echo 13. Enterprise 2016 LTSB
+
+Echo 14. Enterprise 2016 LTSB N
+Echo ___________________________________
+Echo LUTFEN SURUMUNUZUN NUMARASINI GiRiN
+Echo -----------------------------------
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto 1
+
+if %input%==2 goto 2
+
+if %input%==3 goto 3
+
+if %input%==4 goto 4
+
+if %input%==5 goto 5
+
+if %input%==6 goto 6
+
+if %input%==7 goto 7
+
+if %input%==8 goto 8
+
+if %input%==9 goto 9
+
+if %input%==10 goto 10
+
+if %input%==11 goto 11
+
+if %input%==12 goto 12
+
+if %input%==13 goto 13
+
+if %input%==14 goto 14
+
+
+:1
+
+cscript slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:2
+
+cscript slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+PAUSE
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:3
+
+cscript slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:4
+
+cscript slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:5
+
+cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:6
+
+cscript slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:7
+
+cscript slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:8
+
+cscript slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:9
+
+cscript slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+:10
+
+cscript slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+11:
+
+cscript slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+12:
+
+cscript slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+13:
+
+cscript slmgr.vbs /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
+
+14:
+
+cscript slmgr.vbs /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639
+
+timeout /t 10
+
+cscript slmgr.vbs /skms kms.lotro.cc
+
+timeout /t 10
+
+cscript slmgr.vbs /ato
+
+timeout /t 10
+
+cls
+
+Echo _________________________
+echo WINDOWS ANAHTARI YUKLENDi
+Echo -------------------------
+
+PAUSE
+
+EXIT
